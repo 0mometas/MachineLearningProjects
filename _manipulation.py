ed7f5aba4e76cee0e75045b883aed10c6cda6238
Your Name <you@example.com>
2022-04-10 22:51:25 +0700

    Initiate new manipulation code for machine learning

---
+++ _manipulation.py
@@ -0,0 +1,53 @@
+# Manipulation #
+######################
+# 1.0.0:    Initiate #
+######################
+
+########################################################################
+#                            Import library                            #
+########################################################################
+
+import os
+from typing import Union
+
+import pandas as pd
+
+
+########################################################################
+#                           Class Definition                           #
+########################################################################
+class Manipulation:
+
+    @staticmethod
+    def read_data(data: Union[str, pd.DataFrame], **kwargs):
+        """
+        Read the data.
+
+        :param data: [String][Pandas DataFrame]
+        String of path to exist [csv] or [excel] file.
+        String of path to exist directory.
+        The DataFrame
+        :return:
+        DataFrame of data.
+        """
+        if isinstance(data, str):
+            if os.path.isfile(data):
+                pass
+
+            elif os.path.isdir(data):
+                data = os.listdir(data)[-1]
+
+            if data.split(".")[-1].startswith("csv"):
+                data = pd.read_csv(data)
+            elif data.split(".")[-1].startswith("xls"):
+                data = pd.read_excel(data)
+            else:
+                print("Function can only read the csv or excel format.")
+                return None
+        elif isinstance(data, pd.DataFrame):
+            pass
+
+        else:
+            raise ValueError("\"data\" type must be str or pd.DataFrame.")
+
+        return data
